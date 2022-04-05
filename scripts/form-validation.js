28bda44815cd2457a26ce81d28e3d95544940787
Sherry He <xsherryhe@gmail.com>
2022-04-05 19:10:08 -0400

    Added JavaScript form validation functions

---
+++ scripts/form-validation.js
@@ -0,0 +1,39 @@
+const formInputs = document.querySelectorAll('.form-input > input'),
+      password = document.getElementById('password'),
+      passwordConfirm = document.getElementById('pw-confirm');
+
+function setFormEvents() {
+  document.querySelector('button[type="submit"]')
+          .addEventListener('click', () => {
+            addSubmitAttemptClass();
+            checkPasswordMatch();
+          })
+ 
+  formInputs.forEach(input => input.addEventListener('input', removeSubmitAttemptClass));
+  [password, passwordConfirm].forEach(input => input.addEventListener('input', togglePasswordMatchError));
+}
+setFormEvents();
+
+function addSubmitAttemptClass() {
+  formInputs.forEach(input => input.classList.add('submit-attempt'));
+}
+
+function removeSubmitAttemptClass(e) {
+  e.target.classList.remove('submit-attempt');
+}
+
+function checkPasswordMatch() {
+  const passwordError = document.querySelector('#password ~ .error-message'),
+        match = password.value == passwordConfirm.value;
+
+  [password, passwordConfirm].forEach(input => input.setCustomValidity(match ? '' : 'Passwords must match'));
+  passwordError.textContent = match ? 'Password is required' : 'Passwords do not match';
+}
+
+function togglePasswordMatchError() {
+  const fieldsEmpty = !password.value || !passwordConfirm.value;
+  [password, passwordConfirm].forEach(input => {
+    input.classList.remove('submit-attempt');
+    fieldsEmpty ? input.classList.add('hide-validity') : input.classList.remove('hide-validity');
+  })
+}
